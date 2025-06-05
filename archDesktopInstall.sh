0d5ea769a2379346cf1cd9610278da1e6ce1326f
Sykes <74189099+SykesTheLord@users.noreply.github.com>
2025-06-05 18:16:33 +0200

    Update archDesktopInstall.sh

--- archDesktopInstall.sh
+++ archDesktopInstall.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
-sudo pacman -S --noconfirm swaync wireplumber pipewire qt6-wayland qt5-wayland kitty wl-clipboard gnome-keyring libsecret slurp grim imagemagick swappy  spotify-launcher pavucontrol rofi blueman qt6ct bitwarden network-manager-applet nwg-look font-config rustc cargo ghostty linux-headers
+sudo pacman -S --noconfirm swaync
+sudo pacman -S --noconfirm wireplumber
+sudo pacman -S --noconfirm pipewire
+sudo pacman -S --noconfirm qt6-wayland
+sudo pacman -S --noconfirm qt5-wayland
+sudo pacman -S --noconfirm kitty
+sudo pacman -S --noconfirm wl-clipboard
+sudo pacman -S --noconfirm gnome-keyring
+sudo pacman -S --noconfirm libsecret
+sudo pacman -S --noconfirm slurp
+sudo pacman -S --noconfirm grim
+sudo pacman -S --noconfirm imagemagick
+sudo pacman -S --noconfirm swappy
+sudo pacman -S --noconfirm spotify-launcher
+sudo pacman -S --noconfirm pavucontrol
+sudo pacman -S --noconfirm rofi
+sudo pacman -S --noconfirm blueman
+sudo pacman -S --noconfirm qt6ct
+sudo pacman -S --noconfirm bitwarden
+sudo pacman -S --noconfirm network-manager-applet
+sudo pacman -S --noconfirm nwg-look
+sudo pacman -S --noconfirm font-config
+sudo pacman -S --noconfirm rustc
+sudo pacman -S --noconfirm cargo
+sudo pacman -S --noconfirm ghostty
+
 yay -S --noconfirm hyprland-git
 yay -S --noconfirm hyprlock-git
 yay -S --noconfirm hyprpaper-git
